2071ee30fdda188b4dae967293c48679c7764b7c
Fırat Polat <55618506+firatpolat13@users.noreply.github.com>
2020-07-25 02:03:29 +0300

    Add files via upload

---
+++ Basic Calculator.js
@@ -0,0 +1,22 @@
+/*
+Create a function that takes two numbers and a mathematical operator + - / * and will perform a calculation with the given numbers.
+Examples
+
+calculator(2, "+", 2) ➞ 4
+
+calculator(2, "*", 2) ➞ 4
+
+calculator(4, "/", 2) ➞ 2
+
+Notes
+
+If the input tries to divide by 0, return: "Can't divide by 0!"
+*/
+
+function calculator(num1, operator, num2) {
+	if(operator == '+') return num1+num2;
+	if(operator == '-') return num1-num2;
+	if(operator == '*') return num1*num2;
+	if(operator == '/' && num2 != 0) return num1/num2;
+	else return "Can't divide by 0!";
+}
